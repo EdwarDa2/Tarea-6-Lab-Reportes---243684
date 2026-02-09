c0fe9d917326f454722be9afdef14cc2e12122a3
EdwarDa2 <edwardanielgonzalezhernandez@gmail.com>
2026-02-08 22:27:13 -0600

    Eliminar carpeta .next y corregir gitignore

--- tarea6/app-next/.next/dev/server/chunks/ssr/_next-internal_server_app_reports_2_page_actions_f92b7633.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/reports/2/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_reports_2_page_actions_f92b7633.js.map
